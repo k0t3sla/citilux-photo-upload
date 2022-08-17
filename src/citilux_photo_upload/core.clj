6e950f3d274021331a574e1f3b0f65d42d4402c0
k0t3sla <lifa-ali@yandex.ru>
2022-08-17 17:42:54 +0300

    some changes

--- src/citilux_photo_upload/core.clj
+++ src/citilux_photo_upload/core.clj
@@ -26,23 +26,23 @@
 (def articles "Артикула из 1с" (atom []))
 ;;atoms
 
-(defn chck-art 
- "Проверка соответсвия артикула фотографий с артикулами в базе" 
+(defn chck-art
+  "Проверка соответсвия артикула фотографий с артикулами в базе"
   [art]
   (if (some true? (for [x @articles]
                     (= art (:art x))))
     true
     art))
 
-(defn parse-line-1c 
-  "Парсим csv построчтно выкидывая все кроме артикула" 
-  [line] 
+(defn parse-line-1c
+  "Парсим csv построчтно выкидывая все кроме артикула"
+  [line]
   (let [[_ art _ _ _ _]
         (re-matches #"(.*);(.*);(.*);(.*);(.*)" line)]
     {:art art}))
 
 (defn get-articles-1c
- "Получаем все артикула в виде мапы" 
+  "Получаем все артикула в виде мапы"
   []
   (with-open [rdr (-> (io/input-stream (:articles env))
                       (InputStreamReader. "windows-1251")
@@ -54,7 +54,7 @@
   ;; 43200000
   ;; обновляем артикула 2 раза в день 
   (try (get-articles-1c)
-       (catch Exception e 
+       (catch Exception e
          (println (send-message (str "caught exception: Articles file:" (.getMessage e))))
          (send-message (str "caught exception: Articles file:" (.getMessage e)))))
   (<! (timeout 10000))
@@ -80,41 +80,52 @@
       (do (doseq [arg args]
             (fs/copy+ file (str arg path)))
           (io/delete-file file))
-      (do (fs/move file (str eror (fs/base-name file))) 
-          (send-message (str "не верное название файла в папке - " file))))))
+      (do (fs/copy+ file (str eror (fs/base-name file)))
+          (send-message (str "не верное название файла в папке - " file))
+          (io/delete-file file)))))
 
 (defn watch-dirs []
-  (let [hot-dir (list-files (:hot-dir env) "jpg,jpeg,png,mp4")
-        hot-dir-wb (list-files (:hot-dir-wb env) "jpg,jpeg")]
- (try
-    (doseq [file hot-dir-wb]
-      (copy-file file [(:out-wb env)] (:eror-hot-dir env)))
-    (catch Exception e (send-message (str "caught exception: HOT DIR-WB:" (.getMessage e))))) 
-  (try
-    (doseq [file hot-dir]
-      (copy-file file [(:out-web+1c env) (:out-source env)] (:eror-hot-dir env))
-      (add-art-to-queue file))
-    (catch Exception e (send-message (str "caught exception: HOT DIR:" (.getMessage e)))))))
+  (let [hot-dir-wb (list-files (:hot-dir-wb env) "jpg,jpeg")
+        hot-dir (list-files (:hot-dir env) "jpg,jpeg,png,mp4")]
+    (when-not (empty? hot-dir-wb)
+      (try
+        (doseq [file hot-dir-wb]
+          (copy-file file [(:out-wb env)] (:eror-hot-dir env)))
+        (catch Exception e (send-message (str "caught exception: HOT DIR-WB:" (.getMessage e))))))
+    (when-not (empty? hot-dir)
+      (try
+        (doseq [file hot-dir]
+          (copy-file file [(:out-web+1c env) (:out-source env)] (:eror-hot-dir env))
+          ;; добавляем артикула в очередь на выгрузку и уведомление
+          (add-art-to-queue file))
+        (catch Exception e (send-message (str "caught exception: HOT DIR:" (.getMessage e))))))))
+
+
+#_(defn coping-shedule []
+    (let [hot-dir-wb (list-files (:hot-dir-wb env) "jpg,jpeg")
+          hot-dir (list-files (:hot-dir env) "jpg,jpeg,png,mp4")
+          comb (concat hot-dir-wb hot-dir)]))
 
 (defn -main
   []
-  (get-articles-1c)
+  (println "Start watching")
   (loop []
     ;; вечный цикл в котором проверяется таймаут для отправки сообщения, 
     ;; копирование файлов в нужные дирректории и отсылка фото на сайт
     (Thread/sleep 1000)
-    (watch-dirs)
+    (try
+      (watch-dirs)
+      (catch Exception e (println (str "caught exception: " (.getMessage e)))))
     (when (and (not-empty @to-upload)
                (check-timeout @schedule))
-      ;; todo здесь отправка всего и вся
-      (let [articles @to-upload]
-        (reset! to-upload #{})
-        (doseq [art articles]
-          (reset! schedule (l/local-now))
-          (try
-            (upload-fotos art)
-            (catch Exception e (send-message (str "caught exception: " (.getMessage e))))))
-        (notify @new-fotos)
-        (reset! new-fotos #{})
-        (reset! schedule (l/local-now))))
+      ;; здесь отправка всего и вся 
+      (doseq [art @to-upload]
+        (reset! schedule (l/local-now))
+        (try
+          (upload-fotos art)
+          (catch Exception e (send-message (str "caught exception: " (.getMessage e))))))
+      (reset! to-upload #{})
+      (notify @new-fotos)
+      (reset! new-fotos #{})
+      (reset! schedule (l/local-now)))
     (recur)))
